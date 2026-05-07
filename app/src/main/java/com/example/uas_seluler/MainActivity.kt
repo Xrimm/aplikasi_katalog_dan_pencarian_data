73a7605a91629cc5e8f0072cba505c7127733549
Xrimm <hopeof1percent@gmail.com>
2026-05-07 22:08:00 +0800

    fix, create, edit - fix warna data detail class menyatu dengan background - create logika searching dan algoritma sorting - edit data dalam array untuk nama class dan detail class - edit UI/Layout untuk detail class

--- app/src/main/java/com/example/uas_seluler/MainActivity.kt
+++ app/src/main/java/com/example/uas_seluler/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var classList: MutableList<FabulaUltima>
     private lateinit var filteredList: MutableList<FabulaUltima>
     private lateinit var adapter: FabulaUltimaAdapter
+    private lateinit var btnSortAZ: Button
+    private var isSorted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +29,39 @@ class MainActivity : AppCompatActivity() {
         rvCatalog = findViewById(R.id.rvCatalog)
         etSearch = findViewById(R.id.etSearch)
         btnSearch = findViewById(R.id.btnSearch)
+        btnSortAZ = findViewById(R.id.btnSortAZ)
 
         rvCatalog.layoutManager = GridLayoutManager(this, 2)
 
-        // Data dummy coba dulu
         classList = mutableListOf(
             FabulaUltima("Arcanist"),
             FabulaUltima("Esper"),
             FabulaUltima("Wayfarer"),
-            FabulaUltima("Guardian")
+            FabulaUltima("Guardian"),
+            FabulaUltima("Arcanist Variant"),
+            FabulaUltima("Chimerist"),
+            FabulaUltima("Darkblade"),
+            FabulaUltima("Elementalist"),
+            FabulaUltima("Entropist"),
+            FabulaUltima("Fury"),
+            FabulaUltima("Loremaster"),
+            FabulaUltima("Orator"),
+            FabulaUltima("Rogue"),
+            FabulaUltima("Sharpshooter"),
+            FabulaUltima("Spiritist"),
+            FabulaUltima("Tinkerer"),
+            FabulaUltima("Weaponmaster"),
+            FabulaUltima("Dancer"),
+            FabulaUltima("Commander"),
+            FabulaUltima("Chanter"),
+            FabulaUltima("Symbolist"),
+            FabulaUltima("Mutant"),
+            FabulaUltima("Pilot"),
+            FabulaUltima("Necromancer"),
+            FabulaUltima("Floralist"),
+            FabulaUltima("Gourmet"),
+            FabulaUltima("Invoker"),
+            FabulaUltima("Merchant")
         )
 
         filteredList = classList.toMutableList()
@@ -43,21 +69,69 @@ class MainActivity : AppCompatActivity() {
         adapter = FabulaUltimaAdapter(filteredList)
 
         adapter.onItemClick = { selectedItem ->
-            val intent = Intent(this, DetailClass::class.java)
-            intent.putExtra("EXTRA_CLASS", selectedItem.nama)
-            startActivity(intent)
+            val moveIntent = Intent(this, DetailClass::class.java)
+            moveIntent.putExtra("EXTRA_CLASS", selectedItem.nama)
+            startActivity(moveIntent)
         }
 
         rvCatalog.adapter = adapter
 
         btnSearch.setOnClickListener {
-            val keyword = etSearch.text.toString()
+
+            val keyword = etSearch.text.toString().lowercase()
 
             if (TextUtils.isEmpty(keyword)) {
                 etSearch.error = "Mau nyari class apa ni?"
             } else {
                 etSearch.error = null
             }
+
+            filteredList.clear()
+
+            if (keyword.isEmpty()) {
+                filteredList.addAll(classList)
+            } else {
+                for (p in classList) {
+                    if (p.nama.lowercase().contains(keyword)) {
+                        filteredList.add(p)
+                    }
+                }
+            }
+            adapter.notifyDataSetChanged()
+        }
+
+        btnSortAZ.setOnClickListener {
+
+            if (!isSorted) {
+
+                val n = filteredList.size
+
+                for (i in 0 until n - 1) {
+                    for (j in 0 until n - i - 1) {
+
+                        if (filteredList[j].nama >
+                            filteredList[j + 1].nama) {
+
+                            val temp = filteredList[j]
+
+                            filteredList[j] = filteredList[j + 1]
+
+                            filteredList[j + 1] = temp
+                        }
+                    }
+                }
+
+                isSorted = true
+
+            } else {
+
+                filteredList.clear()
+                filteredList.addAll(classList)
+
+                isSorted = false
+            }
+
+            adapter.notifyDataSetChanged()
         }
     }
 }
